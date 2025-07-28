509e5216ca897acd4d22ec4a517a6a2ed75204bd
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-07-28 07:54:06 -0300

    feat: add reusable UI components including Button, Card, Input, and Label
    - Implemented Button component with variants for styling and size.
    - Created Card component with subcomponents: CardHeader, CardTitle, CardDescription, CardAction, CardContent, and CardFooter for structured content display.
    - Developed Input component with customizable styles and accessibility features.
    - Added Label component utilizing Radix UI for enhanced form labeling.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,73 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+
 export default function LoginPage() {
-  return <div>teste page</div>;
+  return (
+    <main className="grid grid-cols-12 items-center min-w-[720px] justify-center min-h-screen bg-blue-100">
+      <div className="grid col-span-6">
+        <Card className="w-full m-auto max-w-sm min-w-sm bg-blue-50 text-blue-600 shadow-lg">
+          <CardHeader className="text-center">
+            <CardTitle>Faça login na sua conta</CardTitle>
+            <CardDescription>
+              Insira seu email abaixo para entrar na sua conta.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    className="bg-white text-gray-900"
+                    placeholder="email@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Senha</Label>
+                    <Link
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Esqueceu sua senha?
+                    </Link>
+                  </div>
+                  <Input
+                    id="password"
+                    placeholder="********"
+                    className="bg-white text-gray-900"
+                    type="password"
+                    required
+                  />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" variant="blue" className="w-full">
+              Login
+            </Button>
+            <Button variant="outlineBlue" className="w-full text-blue-600">
+              Login com Google <FcGoogle />
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="grid w-full h-full col-span-6 m-auto bg-[url('/login_direita.png')] bg-cover bg-no-repeat bg-center shadow-2xl" />
+    </main>
+  );
 }
